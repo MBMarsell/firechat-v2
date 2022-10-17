d83b2383be66a1ddb756007927082cf0920248d5
Martin Marsell <martbustmar@gmail.com>
2022-10-17 17:18:57 +0200

    remove import

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from './context/AuthContext';
 
-import './index.css';
-
 function App() {
   const { currentUser } = useContext(AuthContext);
 
